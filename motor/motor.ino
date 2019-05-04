bf00fe152e6b9b6e8b2fc4688d9bcd92a5366e41
Alperen ARICI <alparicieren@gmail.com>
2019-05-04 13:52:14 +0300

    Arduino Tek e düşürüldü

--- motor/motor.ino
+++ motor/motor.ino
@@ -1,3 +1,17 @@
+#define BLYNK_PRINT Serial
+
+
+#include <SoftwareSerial.h>
+SoftwareSerial SwSerial(2, 3); // RX, TX
+    
+#include <BlynkSimpleSerialBLE.h>
+#include <SoftwareSerial.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "6d352150f478472d9726e76460969aa2";
+SoftwareSerial SerialBLE(2, 3); // RX, TX
+
 const int SagArkaMotor1 = 9;     // Motor sürücümüze bağladığımız pinleri tanımlıyoruz
 const int SagArkaMotor2 = 8;     
 const int SagOnMotor3 =  9;
@@ -8,12 +22,10 @@ const int b =  6;//b ve d birleştirip 6
 const int c =  5;
 const int d =  6;
 
-const int BluetoothIleri =  4;
-const int BluetoothGeri =  12;
+const int BluetoothIleri =  7;
+const int BluetoothSol =  12;
 const int BluetoothSag =  13;
-const int BluetoothSol =  7;
-
-const int OtomatikMode =  3;
+const int OtomatikMode =  4;
 
 const int echo = 10;
 const int trig = 11; 
@@ -22,6 +34,15 @@ int distance;
 
 void setup() 
 {
+
+ // Debug console
+  Serial.begin(9600);
+
+  SerialBLE.begin(9600);
+  Blynk.begin(SerialBLE, auth);
+
+  Serial.println("Waiting for connections...");
+  
 pinMode(SagArkaMotor1, OUTPUT);  //Tüm pinlerden güç çıkışı olacağı için OUTPUT olarak ayarladık.
 pinMode(SagArkaMotor2, OUTPUT);  
 pinMode(SagOnMotor3, OUTPUT);
@@ -32,44 +53,31 @@ pinMode(b, OUTPUT);
 pinMode(c, OUTPUT);
 pinMode(d, OUTPUT);
 pinMode(BluetoothIleri,INPUT);
-pinMode(BluetoothGeri,INPUT);
 pinMode(BluetoothSag,INPUT);
 pinMode(BluetoothSol,INPUT);
-pinMode(OtomatikMode,INPUT);
+
 
 pinMode(trig, OUTPUT); 
 pinMode(echo, INPUT);
 Serial.begin(9600);
 }
 void Dur(){
-  digitalWrite(SagArkaMotor1, HIGH);
-      digitalWrite(SagArkaMotor2,  HIGH);  
-      // motor 2
-      digitalWrite(SagOnMotor3, HIGH);
-      digitalWrite(SagOnMotor4,  HIGH);
-      //motor 3
-      digitalWrite(a, HIGH);
-      digitalWrite(b,  HIGH);
-      //motor 4
-      digitalWrite(c, HIGH);
-      digitalWrite(d,  HIGH);
-  }
-void IleriGit(){
-  // motor 1
-      digitalWrite(SagArkaMotor1, HIGH);
+  
+  
+  digitalWrite(SagArkaMotor1, LOW);
       digitalWrite(SagArkaMotor2,  LOW);  
       // motor 2
-      digitalWrite(SagOnMotor3, HIGH);
+      digitalWrite(SagOnMotor3, LOW);
       digitalWrite(SagOnMotor4,  LOW);
       //motor 3
       digitalWrite(a, LOW);
-      digitalWrite(b,  HIGH);
+      digitalWrite(b,  LOW);
       //motor 4
       digitalWrite(c, LOW);
-      digitalWrite(d,  HIGH);
+      digitalWrite(d,  LOW);
   }
-  void GeriGit(){
-    // motor 1
+void IleriGit(){
+  // motor 1
       digitalWrite(SagArkaMotor1, LOW);
       digitalWrite(SagArkaMotor2,  HIGH);  
       // motor 2
@@ -81,6 +89,20 @@ void IleriGit(){
       //motor 4
       digitalWrite(c, HIGH);
       digitalWrite(d,  LOW);
+  }
+  void GeriGit(){
+    // motor 1
+      digitalWrite(SagArkaMotor1, HIGH);
+      digitalWrite(SagArkaMotor2,  LOW);  
+      // motor 2
+      digitalWrite(SagOnMotor3, HIGH);
+      digitalWrite(SagOnMotor4,  LOW);
+      //motor 3
+      digitalWrite(a, LOW);
+      digitalWrite(b,  HIGH);
+      //motor 4
+      digitalWrite(c, LOW);
+      digitalWrite(d,  HIGH);
   }
   void SagGit(){
     // motor 1
@@ -112,49 +134,27 @@ void IleriGit(){
   }
 void loop() 
 {
-  
+  Dur();
+  Blynk.run();
   if(digitalRead(OtomatikMode)== true){
     IleriGit();
-      //otomatik mod çalışır.
-      // Clears the trigPin
-      digitalWrite(trig, LOW);
-      delayMicroseconds(2);
-      // Sets the trigPin on HIGH state for 10 micro seconds
-      digitalWrite(trig, HIGH);
-      delayMicroseconds(10);
-      digitalWrite(trig, LOW);
-      // Reads the echoPin, returns the sound wave travel time in microseconds
-      duration = pulseIn(echo, HIGH);
-      // Calculating the distance
-      distance= duration*0.034/2;
-      // Prints the distance on the Serial Monitor
-      Serial.print("Distance: ");
-      Serial.println(distance);
-      if(distance <= 20){
-          Dur();
-          delay(200);
-          SagGit();
-          delay(200);
-          
-        } 
-      
-    }
-  else{
-    if(digitalRead(BluetoothIleri) == true){
-      IleriGit();
-    }
-     else if(digitalRead(BluetoothGeri) == true){
-      GeriGit();
-    }
-    else if(digitalRead(BluetoothSag) == true){
+    digitalWrite(trig, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trig, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trig, LOW);
+    duration = pulseIn(echo, HIGH);
+    distance= duration*0.034/2;
+    Serial.print("Distance: ");
+    Serial.println(distance);
+    if(distance <= 20){
+      Dur();
+      delay(200);
       SagGit();
+      delay(200);
+      }
     }
-    else if(digitalRead(BluetoothSol) == true){
-      SolGit();
-    }else{
-       Dur();
+      else if(digitalRead(BluetoothIleri) == true){IleriGit();}
+      else if(digitalRead(BluetoothSol) == true){SolGit();}
+      else if(digitalRead(BluetoothSag) == true){SagGit();}
     }
-    }
-  
-
-}
